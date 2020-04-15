4b9b5a7eb0ccbb65fc014545e884486a2c261a25
zhangxu <2590000714@qq.com>
2020-04-15 18:38:53 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
-      Learn React
+      <Button>默认按钮</Button>
     </div>
   );
 }
