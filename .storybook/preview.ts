800ffbdc80f917cd4f85c6494d6a3b8d636db387
zhangxu <2590000714@qq.com>
2020-04-25 17:26:54 +0800

    🐛fix sotrybook's table style

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,12 @@
 import { addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import '../src/scss/index.scss';
+import './fix-storybook-table.scss';
 
 addDecorator(withInfo);
 addParameters({
   info: {
-    inline: true
+    inline: true,
+    header: false,
   }
 })
