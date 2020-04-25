cbd0c38efb34f08e46bdcfab2c1a78174f6de22f
zhangxu <2590000714@qq.com>
2020-04-25 17:53:01 +0800

    🎉add WrapperStyleDecorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
 import { addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import '../src/scss/index.scss';
 import './fix-storybook-table.scss';
 
+const wrapperStyle: React.CSSProperties = {
+  padding: '20px 40px'
+}
+
+const WrapperStyleDecorator = (storyFn) => <div style={wrapperStyle}>{storyFn()}</div>
+
+addDecorator(WrapperStyleDecorator);
 addDecorator(withInfo);
 addParameters({
   info: {
